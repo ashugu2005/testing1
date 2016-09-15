65db8b574e1e94b184a8e00f040e294c4366f909
ashugu2005 <ashu.gu2005@gmail.com>
2016-09-15 19:28:24 +0530

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -1,4 +1,5 @@
-C:\Program Files (x86)\WiX Toolset v3.10\bin\heat dir C:\Users\vinamdar\Downloads\Vasim\SVM.Shared.API.Products -o MyHarvestfile.wxs -cg MyComponentGroupId -gg -scom -sreg -sfrag -srd -dr level2 -var var.SourceDir
-C:\Program Files (x86)\WiX Toolset v3.10\bin\candle -dSourceDir="C:\Users\vinamdar\Downloads\Vasim\SVM.Shared.API.Products" MyHarvestfile.wxs SVM.Shared.API.Products.wxs   -ext WixIIsExtension -ext WixUtilextension  -ext WixUIextension
-C:\Program Files (x86)\WiX Toolset v3.10\bin\light MyHarvestfile.wixobj SVM.Shared.API.Products.wixobj  -o SVM.Shared.API.Products.msi -ext WixIIsExtension -ext WixUtilextension  -ext WixUIextension
+set wix_installer_loc="C:\Program Files (x86)\WiX Toolset v3.10\bin"
+%wix_installer_loc%\heat dir C:\Users\vinamdar\Downloads\Vasim\SVM.Shared.API.Products -o MyHarvestfile.wxs -cg MyComponentGroupId -gg -scom -sreg -sfrag -srd -dr level2 -var var.SourceDir
+%wix_installer_loc%C:\Program Files (x86)\WiX Toolset v3.10\bin\candle -dSourceDir="C:\Users\vinamdar\Downloads\Vasim\SVM.Shared.API.Products" MyHarvestfile.wxs SVM.Shared.API.Products.wxs   -ext WixIIsExtension -ext WixUtilextension  -ext WixUIextension
+%wix_installer_loc%C:\Program Files (x86)\WiX Toolset v3.10\bin\light MyHarvestfile.wixobj SVM.Shared.API.Products.wixobj  -o SVM.Shared.API.Products.msi -ext WixIIsExtension -ext WixUtilextension  -ext WixUIextension
 @pause
